11b8ae5cca39b9b2f232df3384f6827b77531327
Mayank Kushal <mayank.kushal@meragi.com>
2025-10-14 21:53:20 +0530

    Initial Commit

---
+++ flow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  flow
+//
+//  Created by Mayank Kushal on 14/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ flow/flowApp.swift
@@ -0,0 +1,17 @@
+//
+//  flowApp.swift
+//  flow
+//
+//  Created by Mayank Kushal on 14/10/25.
+//
+
+import SwiftUI
+
+@main
+struct flowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
